26e543084cd0bf667f1e58f9281547186cf24ee7
slvgs <gabriellasilveriosantana15@gmail.com>
2023-02-24 12:16:56 -0300

    Feito os DTOs

---
+++ src/dtos/userDTO.ts
@@ -0,0 +1,54 @@
+import { PostModel } from "../types"
+
+
+
+//DTO é uma ponte para entradas e saidas, com valores "burros", para ser "customizado" na regra de negócio; 
+
+
+
+export interface SingupInputDTO{
+    name:unknown,
+    email:unknown,
+    password:unknown
+}
+
+export interface SingupOutputDTO{
+    token: string
+}
+
+
+export interface LoginInputDTO{
+    email: unknown,
+    password: unknown
+}
+
+export interface LoginOutputDTO{
+    token: string
+}
+
+export interface GetPostsInputDTO{
+    token: string | undefined
+}
+
+export type GetPostOutputDTO = PostModel[]
+
+export interface CreatePostInputDTO{
+    token: string | undefined
+}
+
+export interface EditPostInputDTO{
+    idToEdit: string,
+    token: string | undefined,
+    name: unknown
+}
+
+export interface DeletePostInputDTO{
+    idToDelete: string,
+    token: string | undefined
+}
+
+export interface LikeOrDislikeInputDTO{
+    idToLikeDislike: string,
+    token: string | undefined,
+    like: unknown
+}
